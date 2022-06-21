cb063a06155c97ff3ca28b541df6719f76fd52d5
feddericahu99 <federica.hu339@gmail.com>
2022-06-21 17:29:29 +0200

    rounded results

--- js/script.js
+++ js/script.js
@@ -15,31 +15,21 @@ const etaViaggiatore = prompt('Quanti anni hai?');
 console.log(etaViaggiatore);
 
 /* Prezzo iniziale */
-const costoIniziale = (kmPercorrenza * 0.21);
+const costoIniziale = (kmPercorrenza * 0.21).toFixed(2);
 console.log(costoIniziale);
 
 /* Mostro il prezzo all'user */
 document.getElementById('prezzo-non-scontato').innerText = '€' + costoIniziale;
 
+/* Applico sconti e mostro numeri con solo due decimali dopo la virgola */
 
+if ( etaViaggiatore < 18 ) {
+    let prezzoGiovani = (costoIniziale * 0.8);
+    console.log(prezzoGiovani);
+    document.getElementById('nuovo-prezzo').innerText = 'Nuovo prezzo scontato:' + '  ' + '€' + prezzoGiovani.toFixed(2);
 
-/* Applico sconti */
-
-if ( etaViaggiatore < 18 || 65 < etaViaggiatore) {
-    if ( etaViaggiatore < 18 ) {
-        const scontoMinorenni = costoIniziale * 20 / 100;
-        console.log(scontoMinorenni);
-        let prezzoGiovani = (costoIniziale - scontoMinorenni);
-        console.log(prezzoGiovani);
-        document.getElementById('nuovo-prezzo').innerText = 'Nuovo prezzo scontato:' + '  ' + '€' + prezzogiovani;
-
-    } else if ( 65 < etaViaggiatore ) {
-        const scontoOver = costoIniziale * 40 / 100;
-        console.log(scontoOver);
-        let prezzoOver = (costoIniziale - scontoOver);
-        console.log(prezzoOver);
-        document.getElementById('nuovo-prezzo').innerText = 'Nuovo prezzo scontato:' + '  ' + '€' + prezzoOver ;
-    }
+} else if ( 65 < etaViaggiatore ) {
+    let prezzoOver = (costoIniziale * 0.6 );
+    console.log(prezzoOver);
+    document.getElementById('nuovo-prezzo').innerText = 'Nuovo prezzo scontato:' + '  ' + '€' + prezzoOver.toFixed(2);
 }
-
-/* Mostro il prezzo nella pagina con due decimali. */
