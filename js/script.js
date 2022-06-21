4535e5c0b7cc9e810d6c6868234768640c961fd9
feddericahu99 <federica.hu339@gmail.com>
2022-06-21 15:30:19 +0200

    created file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+1- Chiedo quanti chilometri deve percorrere.
+2- Chiedo quanti hanni l'età del passeggiero.
+3- Prezzo iniziale
+4- Applicare sconti.
+5- Mostro il prezzo nella pagina con due deccimali.
+*/
+
